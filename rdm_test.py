6db364a2a0c8ad1c7e6beff2d07a0cc9b9cef5a2
Scott Smart <sesmart@uchicago.edu>
2019-02-26 12:19:46 -0600

    rawr

--- rdm_test.py
+++ rdm_test.py
@@ -5,24 +5,24 @@ mol = gto.Mole()
 mol.atom=[
         ['H',(0,0,0)],
         ['H',(1.0,0,0)]]
-#mol.basis='sto-3g'
-mol.basis = '6-31g'
+mol.basis='sto-3g'
+#mol.basis = '6-31g'
 mol.spin=0
 mol.build()
 mol.as_Ne = 2
-mol.as_No = 4 #spatial
+mol.as_No = 2 #spatial
 
 
 prog = sp(mol,'rdm',calc_E=True)
 kw_qc = {
         'Nqb':mol.as_No*2,
-        'tomo_basis':'hada',
+        'tomo_basis':'hada+imag',
         'spin_mapping':'default',
         #'entangler_p':'Uent1_cN',
         #'entangled_pairs':'scheme1_Tavernelli',
         'entangler_p':'UCC1',
-        'entangler_q':'UCC2s',
-        'entangled_pairs':'d',
+        'entangler_q':'UCC2',
+        'entangled_pairs':'sd',
         'ansatz':'ucc',
         'pr_q':1,
         'depth':1,
@@ -31,7 +31,7 @@ kw_qc = {
 kw_opt = {
         'optimizer':'nevergrad',
         'nevergrad_opt':'Cobyla',
-        'max_iter':200
+        'max_iter':500
         }
 prog.update_var(target='qc',**kw_qc )
 prog.update_var(target='opt',**kw_opt)
