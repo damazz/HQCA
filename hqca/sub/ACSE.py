e6a52d8c248745818ef7463b8c13745e7ef4f7a1
rawrses <smartscott@gmail.com>
2019-10-03 12:45:06 -0500

    auto changes

--- hqca/sub/ACSE.py
+++ hqca/sub/ACSE.py
@@ -111,10 +111,10 @@ class RunACSE(QuantumRun):
         testS = quantS.findSPairsQuantum(self.Store,self.QuantStore)
         self._check_norm(testS)
         if self.method=='qq-acse':
-            self.delta = 0.20
+            self.delta = 0.40
             self.__euler_qc_acse(testS)
         elif self.method=='qq-acse2':
-            self.delta = 0.5
+            self.delta = 1.0
             self.__newton_qc_acse(testS)
 
     def _check_norm(self,testS):
