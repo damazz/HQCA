0f32c2b7879031ecc971d88bb4204ef04cad6903
sesmart <smartscott@gmail.com>
2021-08-19 12:11:10 -0500

    changes to ACSE keywords

--- hqca/acse/_class_S_acse.py
+++ hqca/acse/_class_S_acse.py
@@ -47,9 +47,6 @@ def findSPairs(
     # 
     #
     A = np.zeros((No,No,No,No),dtype=np.complex_)
-    temp1 = np.zeros((No,No,No,No))
-    #temp2 = np.zeros((N,N,N,N))
-    #temp3 = np.zeros((N,N,N,N))
     #K1 = store.H.ints_1e
     #K2 = store.H.ints_2e
     #W = K2 - K2.transpose(0,1,3,2)
@@ -59,50 +56,13 @@ def findSPairs(
         for j in so:
             for k in so:
                 for l in so:
-                    #if i==0 and j==1 and k==2 and l==3:
-                    #    print(0.5*np.dot(D2[:,:,j,l],W[:,:,i,k].T).trace())
-                    #    print( 0.5 * np.dot(D2[:, :, i, k], W[:, :, j, l].T).trace())
-                    #    A1,A2 = D2[:,:,j,l], D2[:,:,i,k]
-                    #    B1,B2 = W[:,:,i,k].T, W[:,:,j,l].T
-                    #    for p in so:
-                    #        for q in so:
-                    #            if abs(D2[p,q,j,l])>1e-5:
-                    #                print(p,q,l,j,D2[p,q,j,l],p,q,k,i, W[p,q,i,k])
-                    #            if abs(D2[p, q, i, k]) > 1e-5:
-                    #                print(p, q, k,i, D2[p, q, i, k], p,q,l,j,W[p, q, j, l])
-
-                    #    print(D2[:,:,j,l])
-                    #    print(D2[:,:,i,k])
-                    #    print(W[:,:,i,k].T)
-                    #    print(W[:,:,j,l].T)
-
-                    #for p in so:
-                    #    for r in so:
-                    #        A[i,k,j,l]-= D2[p,r,j,l]*W[p,r,i,k]
-                    #        A[i,k,j,l]+= D2[p,r,i,k]*W[p,r,j,l]
                     A[i,k,j,l]-= 0.5*np.dot(D2[:,:,j,l],W[:,:,i,k].T).trace()
                     A[i,k,j,l]+= 0.5*np.dot(D2[:,:,i,k],W[:,:,j,l].T).trace()
-                    #
-                    #A[i,k,j,l]-= np.dot(K1[:,i],D2[:,k,j,l].T)
-                    #A[i,k,j,l]+= np.dot(K1[:,k],D2[:,i,j,l].T)
-                    #A[i,k,j,l]-= np.dot(K1[:,l],D2[:,j,i,k].T)
-                    #A[i,k,j,l]+= np.dot(K1[:,j],D2[:,l,i,k].T)
                     for r in so:
-                        #if i == 0 and j == 1 and k == 2 and l == 3:
-                        #    print(r,0.5*np.dot(D3[:,r,k,j,l,:],W[:,r,i,:].T).trace() )
-                        #    print(r,0.5*np.dot(D3[:,r,k,j,l,:],W[:,r,i,:].T).trace())
-                        #    print(r, 0.5*np.dot(D3[i,k,:,r,:,j],W[:,l,r,:].T).trace())
-                        #    print(r,0.5*np.dot(D3[i,k,:,r,:,l],W[:,j,r,:].T).trace())
                         A[i,k,j,l]+= 0.5*np.dot(D3[:,r,k,j,l,:],W[:,r,i,:].T).trace()
                         A[i,k,j,l]-= 0.5*np.dot(D3[:,r,i,j,l,:],W[:,r,k,:].T).trace()
                         A[i,k,j,l]-= 0.5*np.dot(D3[i,k,:,r,:,j],W[:,l,r,:].T).trace()
                         A[i,k,j,l]+= 0.5*np.dot(D3[i,k,:,r,:,l],W[:,j,r,:].T).trace()
-                        #A[i,k,j,l]+= 0.5*np.dot(D3[p,r,k,j,l,q],W[p,r,i,q].T).trace() 
-                        #A[i,k,j,l]-= 0.5*np.dot(D3[p,r,i,j,l,q],W[p,r,k,q].T).trace()
-                        #A[i,k,j,l]-= 0.5*np.dot(D3[i,k,p,r,q,j],W[:,l,r,:].T).trace()
-                        #A[i,k,j,l]+= 0.5*np.dot(D3[i,k,p,r,q,l],W[:,j,r,:].T).trace()
-                    #if abs(A[i,k,j,l])>1e-5:
-                    #    print(A[i,k,j,l], i, k, l, j)
     nz = np.nonzero(A)
     new = Operator()
     for i,k,j,l in zip(nz[0],nz[1],nz[2],nz[3]):
--- hqca/acse/_quant_S_acse.py
+++ hqca/acse/_quant_S_acse.py
@@ -5,184 +5,133 @@ from hqca.operators import *
 from hqca.tomography import *
 import traceback
 from timeit import default_timer as dt
+from copy import deepcopy as copy
+import warnings
 
-'''
-/hqca/acse/_quant_S_acse.py
 
-Will generate elements of the A matrix according to the quantum solution. Requires tomography of the auxillary 2-RDM, aquired with an additional propagator sequence appended to the ansatz. 
-
-'''
-
-def findSPairsQuantum(
-        op_type,
+def solveqACSE(
+        H=None,
+        S_min=1e-6,
+        expiH_approx='first',
+        matrix=False,
+        verbose=False,
         **kw
         ):
-    newS = _findFermionicSQuantum(**kw)
-    return newS
+    '''
+    provides a solution to the qACSE
 
-def _findFermionicSQuantum(
+    expiH_approx refers to a first or second order approximation in e^iHx 
+    H indicates the Hamiltonian operator provided
+
+    TODO: Change imag/real tomography to be system specific or implied from the Hamiltonian operator (currently second order is redundant)
+
+    additionally, we can perform a summand over many H operators
+    '''
+    kw['matrix']=matrix
+    if type(H)==type(None):
+        raise HamiltoninError('Cannot run qACSE without a qubit Hamiltonian.')
+    elif isinstance(H,type(Operator())):
+        if expiH_approx=='first':
+            rdm = _runexpiH(HamiltonianOperator=H,**kw)
+        elif expiH_approx=='second':
+            warnings.warn('Second order method is equivalent to first order for the real case!')
+            rdm1 = _runexpiH(HamiltonianOperator=H,**kw)
+            Hp = copy(H)*(-1)
+            rdm2 = _runexpiH(HamiltonianOperator=Hp,**kw)
+            rdm = (rdm1-rdm2)/2
+    elif isinstance(H,list):
+        if expiH_approx=='first':
+            rdm = _runexpiH(HamiltonianOperator=H[0],**kw)
+            for h in H[1:]:
+                rdm+= _runexpiH(HamiltonianOperator=h,**kw)
+        elif expiH_approx=='second':
+            warnings.warn('Second order method is equivalent to first order for the real case!')
+            rdm1 = _runexpiH(HamiltonianOperator=H[0],**kw)
+            for h in H[1:]:
+                rdm1+= _runexpiH(HamiltonianOperator=h,**kw)
+            Hp = H*(-1)
+            rdm2 = _runexpiH(HamiltonianOperator=Hp[0],**kw)
+            for h in H[1:]:
+                rdm2 += _runexpiH(HamiltonianOperator=h,**kw)
+            rdm = (rdm1-rdm2)/2
+    if matrix:
+        return rdm
+    else:
+        # form fermionic operator
+        if verbose:
+            print('Elements of S from quantum generation: ')
+        newF = Operator()
+        nz = np.transpose(np.nonzero(rdm))
+        for index in nz:
+            ind = tuple(index)
+            val = rdm[ind]
+            if abs(val)>S_min:
+                l = len(ind)
+                sop = l//2*'+'+l//2*'-'
+                newF+= FermiString(
+                        -val,
+                        indices=list(ind),
+                        ops=sop,
+                        N=rdm.shape[0],
+                        )
+        if verbose:
+            print('S operator (pre-truncated)...')
+            print('Fermionic S operator:')
+            print(newF)
+        return newF
+
+def _runexpiH(
         operator=None,
         instruct=None,
         process=None,
         store=None,
         quantstore=None,
         verbose=False,
-        trotter_steps=1,
         S_min=1e-10,
-        hamiltonian_step_size=1.0,
-        ordering='default',
-        depth=1,
-        commutative=True,
+        hamiltonian_step_size=0.1,
         tomo=None,
         transform=None,
-        propagate='first',
         matrix=False,
         **kw
         ):
     '''
-    need to do following:
-        3. find S from resulting matrix
-
+    subroutine for running an exp iO circuit where O is an operator
     '''
     t0 = dt()
-    if propagate=='first':
-        if verbose:
-            print('Generating new S pairs with Hamiltonian step.')
-        newPsi = instruct(
-                operator=operator,
-                Nq=quantstore.Nq,
-                quantstore=quantstore,
-                propagate=True,
-                HamiltonianOperator=store.H.qubit_operator,
-                scaleH=hamiltonian_step_size,
-                depth=depth,
-                **kw
-                )
-        #('-- ins A: {}'.format(dt() - t0))
-        t0 = dt()
-        if type(tomo)==type(None):
-            print('Recalculating? ')
-            newCirc = StandardTomography(
-                    quantstore,
-                    verbose=verbose,
-                    )
-            newCirc.generate(real=False,imag=True)
-        else:
-            newCirc = StandardTomography(
-                    quantstore,
-                    preset=True,
-                    Tomo=tomo,
-                    verbose=verbose,
-                    )
-        #print('-- tomo A: {}'.format(dt() - t0))
-        t0 = dt()
-        newCirc.set(newPsi)
-        #print('-- set A: {}'.format(dt()-t0))
-        t0 = dt()
-        if verbose:
-            print('Running circuits...')
-        newCirc.simulate(verbose=verbose)
-        #print('-- sim A: {}'.format(dt()-t0))
-        t0 = dt()
-        if verbose:
-            print('Constructing the RDMs...')
-        hss = (1 / hamiltonian_step_size)
-        if matrix:
-            newCirc.construct(processor=process,compact=True)
-            rdm = np.imag(newCirc.rdm) * hss
-        else:
-            newCirc.construct(processor=process)
-            rdm = np.imag(newCirc.rdm.rdm) * hss
-
-        #print('-- construct A: {}'.format(dt()-t0))
-        t0 = dt()
-        #rdm = newCirc.rdm.rdm-store.rdm.rdm
-    elif propagate=='second':
-        Psi1 = instruct(
-                operator=operator,
-                Nq=quantstore.Nq,
-                quantstore=quantstore,
-                propagate=True,
-                HamiltonianOperator=store.H.qubit_operator,
-                scaleH=hamiltonian_step_size,
-                depth=depth,
-                **kw
-                )
-        H2 = store.H.qubit_operator*(-1)
-        Psi2 = instruct(
-                operator=operator,
-                Nq=quantstore.Nq,
-                quantstore=quantstore,
-                propagate=True,
-                HamiltonianOperator=H2,
-                scaleH=hamiltonian_step_size,
-                depth=depth,
-                **kw
-                )
-        if type(tomo)==type(None):
-            Circ1 = StandardTomography(
-                    quantstore,
-                    verbose=verbose,
-                    )
-            Circ1.generate(real=False,imag=True)
-            Circ2 = StandardTomography(
-                    quantstore,
-                    verbose=verbose,
-                    )
-            Circ2.generate(real=False,imag=True)
-        else:
-            Circ1 = StandardTomography(
-                    quantstore,
-                    preset=True,
-                    Tomo=tomo,
-                    verbose=verbose,
-                    )
-            Circ2 = StandardTomography(
-                    quantstore,
-                    preset=True,
-                    Tomo=tomo,
-                    verbose=verbose,
-                    )
-        Circ1.set(Psi1)
-        Circ2.set(Psi2)
-        if verbose:
-            print('Running circuits...')
-        Circ1.simulate(verbose=verbose)
-        Circ2.simulate(verbose=verbose)
-        if verbose:
-            print('Constructing the RDMs...')
-        Circ1.construct(processor=process)
-        Circ2.construct(processor=process)
-        #rdm = newCirc.rdm.rdm-store.rdm.rdm
-        hss = (1/(2*hamiltonian_step_size))
-        rdm = -np.imag(-Circ1.rdm.rdm+Circ2.rdm.rdm)*hss
-    #print('Time to run A: {}'.format(dt()-t0))
+    if verbose:
+        print('Generating new S pairs with Hamiltonian step.')
+    newPsi = instruct(
+            operator=operator,
+            Nq=quantstore.Nq,
+            quantstore=quantstore,
+            scaleH=hamiltonian_step_size,
+            propagate=True,
+            **kw
+            )
     t0 = dt()
+    if type(tomo)==type(None):
+        print('Recalculating tomography....')
+        newCirc = StandardTomography(
+                quantstore,
+                verbose=verbose,
+                )
+        newCirc.generate(real=False,imag=True)
+    else:
+        newCirc = StandardTomography(
+                quantstore,
+                preset=True,
+                Tomo=tomo,
+                verbose=verbose,
+                )
+    newCirc.set(newPsi)
+    newCirc.simulate(verbose=verbose)
+    hss = (1 / hamiltonian_step_size)
     if matrix:
-        return rdm
-    if verbose:
-        print('Elements of S from quantum generation: ')
-    newF = Operator()
-    new = np.transpose(np.nonzero(rdm))
-    for index in new:
-        ind = tuple(index)
-        val = rdm[ind]
-        if abs(val)>S_min:
-            l = len(ind)
-            sop = l//2*'+'+l//2*'-'
-            newF+= FermiString(
-                    -val,
-                    indices=list(ind),
-                    ops=sop,
-                    N=quantstore.dim,
-                    )
-    #print('Time to process A: {}'.format(dt()-t0))
-    t0 = dt()
-    #fullS = newF.transform(quantstore.transform)
-    if verbose:
-        print('S operator (pre-truncated)...')
-        print('Fermionic S operator:')
-        print(newF)
-    #print('Time to transform A: {}'.format(dt()-t0))
-    return newF
+        newCirc.construct(processor=process,compact=True)
+        rdm = np.imag(newCirc.rdm) * hss
+    else:
+        newCirc.construct(processor=process)
+        rdm = np.imag(newCirc.rdm.rdm) * hss
+    return rdm
+
 
